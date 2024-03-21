5a593b07ad340d4d5bf28894911a69733fff423c
Prechak <benedict.precha@gmail.com>
2024-03-22 02:02:58 +0700

    Comment not relate part

--- Reskill_Challenge/starter/09m-Omnifood-Optimizations/js/script.js
+++ Reskill_Challenge/starter/09m-Omnifood-Optimizations/js/script.js
@@ -6,11 +6,11 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-h1.addEventListener("click", function () {
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
-});
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
