23195ad9b036852d2c8db143cf512fd2a41a5ef5
Prechak <benedict.precha@gmail.com>
2024-03-22 13:09:22 +0700

    Implement smooth scrolling

--- Reskill_Challenge/starter/09m-Omnifood-Optimizations/js/script.js
+++ Reskill_Challenge/starter/09m-Omnifood-Optimizations/js/script.js
@@ -12,10 +12,50 @@ console.log(h1);
 //   h1.style.padding = "5rem";
 // });
 
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault;
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", //not work anymore but work in safari with extension
+      });
+
+    //scroll to other navigation
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
